8586d247a0e6f57fb09b1c9868f8954c25fd497d
Vladislav <tsekov.vladislav@gmail.com>
2023-08-31 22:14:26 +0300

    Created properties for the IMotorcycle interface.

--- TwoWheelTrader/Models/Interfaces/IMotorcycle.cs
+++ TwoWheelTrader/Models/Interfaces/IMotorcycle.cs
@@ -2,6 +2,17 @@
 {
     public interface IMotorcycle
     {
-
+        string Make { get; }
+        string Model { get; }
+        int CC { get; }
+        int Year { get; }
+        int PriceForeign { get; }
+        int PriceBGN { get; }
+        int MarketPrice { get; }
+        int DistanceToPickUp { get; }
+        int FuelCost { get; }
+        int TotalCost { get; }
+        int Profit { get; }
+        string Link { get; }
     }
 }
