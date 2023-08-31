4df896c3dd6ee75f8c07e27cbeb7e89cfb123de8
Vladislav <tsekov.vladislav@gmail.com>
2023-09-01 00:32:27 +0300

    Updated Enduro Yamaha MarketPrices using WebScraperV1.

--- TwoWheelTrader/Models/Enduro/Yamaha.cs
+++ TwoWheelTrader/Models/Enduro/Yamaha.cs
@@ -16,39 +16,39 @@ namespace TwoWheelTrader.Models.Enduro
             switch (Year)
             {
                 case 2007:
-                    MarketPrice = 5000; break;
-                case 2008:
-                    MarketPrice = 5100; break;
-                case 2009:
-                    MarketPrice = 5200; break;
-                case 2010:
-                    MarketPrice = 5300; break;
-                case 2011:
-                    MarketPrice = 5600; break;
-                case 2012:
-                    MarketPrice = 5900; break;
-                case 2013:
                     MarketPrice = 6200; break;
-                case 2014:
-                    MarketPrice = 6600; break;
-                case 2015:
-                    MarketPrice = 6800; break;
-                case 2016:
+                case 2008:
+                    MarketPrice = 6400; break;
+                case 2009:
+                    MarketPrice = 6700; break;
+                case 2010:
+                    MarketPrice = 6900; break;
+                case 2011:
                     MarketPrice = 7300; break;
-                case 2017:
+                case 2012:
+                    MarketPrice = 7600; break;
+                case 2013:
                     MarketPrice = 7900; break;
+                case 2014:
+                    MarketPrice = 8400; break;
+                case 2015:
+                    MarketPrice = 8700; break;
+                case 2016:
+                    MarketPrice = 9000; break;
+                case 2017:
+                    MarketPrice = 9300; break;
                 case 2018:
-                    MarketPrice = 8500; break;
+                    MarketPrice = 9600; break;
                 case 2019:
-                    MarketPrice = 9200; break;
-                case 2020:
                     MarketPrice = 10000; break;
-                case 2021:
-                    MarketPrice = 10500; break;
-                case 2022:
+                case 2020:
                     MarketPrice = 11000; break;
-                case 2023:
+                case 2021:
                     MarketPrice = 12000; break;
+                case 2022:
+                    MarketPrice = 13000; break;
+                case 2023:
+                    MarketPrice = 14000; break;
             }
         }
         public string Make { get; set; }
