2c92aff69e0c0b4690770a68880a3706ee52ee47
Vladislav <tsekov.vladislav@gmail.com>
2023-08-31 22:23:43 +0300

    Implemented "IMotocross/Honda" class.

--- TwoWheelTrader/Models/Motocross/Honda.cs
+++ TwoWheelTrader/Models/Motocross/Honda.cs
@@ -4,6 +4,73 @@ namespace TwoWheelTrader.Models.Motocross
 {
     public class Honda : IMotocross
     {
+        public Honda(string model, int cc, int year, int priceForeign, int priceBGN)
+        {
+            Make = "Honda";
+            Model = model;
+            CC = cc;
+            Year = year;
+            PriceForeign = priceForeign;
+            PriceBGN = priceBGN;
 
+            switch (Year)
+            {
+                case 2008:
+                    MarketPrice = 5050; break;
+                case 2009:
+                    MarketPrice = 5100; break;
+                case 2010:
+                    MarketPrice = 5600; break;
+                case 2011:
+                    MarketPrice = 5800; break;
+                case 2012:
+                    MarketPrice = 6300; break;
+                case 2013:
+                    MarketPrice = 6500; break;
+                case 2014:
+                    MarketPrice = 7000; break;
+                case 2015:
+                    MarketPrice = 7150; break;
+                case 2016:
+                    MarketPrice = 7500; break;
+                case 2017:
+                    MarketPrice = 8200; break;
+                case 2018:
+                    MarketPrice = 8650; break;
+                case 2019:
+                    MarketPrice = 9500; break;
+                case 2020:
+                    MarketPrice = 10300; break;
+                case 2021:
+                    MarketPrice = 10800; break;
+                case 2022:
+                    MarketPrice = 11200; break;
+                case 2023:
+                    MarketPrice = 12000; break;
+            }
+        }
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int CC { get; set; }
+
+        public int Year { get; set; }
+
+        public int PriceForeign { get; set; }
+
+        public int PriceBGN { get; set; }
+
+        public int MarketPrice { get; set; }
+
+        public int DistanceToPickUp { get; set; }
+
+        public int FuelCost { get; set; }
+
+        public int TotalCost { get; set; }
+
+        public int Profit { get; set; }
+
+        public string Link { get; set; }
     }
 }
