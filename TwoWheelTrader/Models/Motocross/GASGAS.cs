c1695b41e811680d013a10d0cd689eba9363ae4d
Vladislav <tsekov.vladislav@gmail.com>
2023-09-02 11:25:31 +0300

    Implemented "IMotocross/GASGAS" class.

--- TwoWheelTrader/Models/Motocross/GASGAS.cs
+++ TwoWheelTrader/Models/Motocross/GASGAS.cs
@@ -4,6 +4,48 @@ namespace TwoWheelTrader.Models.Motocross
 {
     public class GASGAS : IMotocross
     {
+        public GASGAS(string model, int cc, int year, int priceForeign, int priceBGN)
+        {
+            Make = "GASGAS";
+            Model = model;
+            CC = cc;
+            Year = year;
+            PriceForeign = priceForeign;
+            PriceBGN = priceBGN;
 
+            switch (Year)
+            {
+                case 2021:
+                    MarketPrice = ; break;
+                case 2022:
+                    MarketPrice = ; break;
+                case 2023:
+                    MarketPrice = ; break;
+            }
+        }
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int CC { get; set; }
+
+        public int Year { get; set; }
+
+        public int PriceForeign { get; set; }
+
+        public int PriceBGN { get; set; }
+
+        public int MarketPrice { get; set; }
+
+        public int DistanceToPickUp { get; set; }
+
+        public int FuelCost { get; set; }
+
+        public int TotalCost { get; set; }
+
+        public int Profit { get; set; }
+
+        public string Link { get; set; }
     }
 }
+
