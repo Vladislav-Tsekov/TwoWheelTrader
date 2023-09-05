3e3c405534f5287e0bd93429c74c5fd90cbdf7e0
Vladislav <tsekov.vladislav@gmail.com>
2023-09-05 16:30:40 +0300

    Implemented two new methods in the Repository interface.

--- TwoWheelTrader/Repositories/Interfaces/IRepository.cs
+++ TwoWheelTrader/Repositories/Interfaces/IRepository.cs
@@ -6,6 +6,10 @@
 
         void AddMotorcycle(T motorcycle);
 
+        void TopThreeByProfit();
+
+        void TopThreeROI();
+
         //void RemoveMotorcycle(string motorcycle); -- Not yet implemented, signature unknown, probably an ID
 
         //void MotorcycleExists(T motorcycle); -- Not sure if this will be needed for the scope of the program, will just add it for now
