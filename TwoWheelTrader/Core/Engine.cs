f78ce856b3bd97a39923f2b2b8a167c9ceb706a6
Vladislav <tsekov.vladislav@gmail.com>
2023-09-05 16:24:19 +0300

    Made some minor suggestions as to how the input should be classified.

--- TwoWheelTrader/Core/Engine.cs
+++ TwoWheelTrader/Core/Engine.cs
@@ -15,15 +15,30 @@ namespace TwoWheelTrader.Core
                     string output = string.Empty;
                     string command = input[0];
 
-                    if (input[0] == "done")
+                    while (true)
                     {
-                        Environment.Exit(0);
+                        if (command == "done")
+                        {
+                            Environment.Exit(0);
+                        }
+                        else if (command == "add")
+                        {
+                            // 1 = YAMAHA / 2 = YZF / 3 = 250 / 4 = 2019 / 5 = 33000 (FOREIGN PRICE - PRE-CONVERT)
+                        }
+                        else if (command == "remove")
+                        {
+                            // Once the program is connected to the MS SQL this command should become a viable option
+                        }
+                        else if (command == "update")
+                        {
+                            // When a change of some property is needed, usually the price - when the motorcycle has had a discount
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid command. Try again!");
+                        }
+                        // TO ADD MORE ELSE IFS BASED ON DIFFERENT SCENARIOS
                     }
-                    else if (input[0] == "check")
-                    {
-
-                    }
-                    // TO ADD MORE ELSE IFS BASED ON DIFFERENT SCENARIOS
                 }
                 catch (Exception ex)
                 {
