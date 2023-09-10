2a94e934e2042f640a8d98722137e94c17049a2b
Vladislav <tsekov.vladislav@gmail.com>
2023-09-10 22:21:08 +0300

    Implemented the repositories in the controller class.

--- TwoWheelTrader/Core/Controller.cs
+++ TwoWheelTrader/Core/Controller.cs
@@ -1,10 +1,26 @@
 ﻿using TwoWheelTrader.Core.Interfaces;
 using TwoWheelTrader.Models.Interfaces;
+using TwoWheelTrader.Repositories;
 
 namespace TwoWheelTrader.Core
 {
     public class Controller : IController
     {
+        private EnduroRepository enduro;
+        private MotocrossRepository motocross;
+        private NakedRepository naked;
+        private SportRepository sport;
+        private TourerRepository tourer;
+
+        public Controller()
+        {
+            enduro = new EnduroRepository();
+            motocross = new MotocrossRepository();
+            naked = new NakedRepository();
+            sport = new SportRepository();
+            tourer = new TourerRepository();
+        }
+
         public string AddMotorcycle(IMotorcycle motorcycle)
         {
             throw new NotImplementedException();
