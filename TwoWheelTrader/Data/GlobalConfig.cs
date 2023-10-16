8daff4d85288ef09b1dc36e2db943799633ef24d
Vladislav <tsekov.vladislav@gmail.com>
2023-10-16 22:09:25 +0300

    Implemented "GlobalConfig" class in the Data section.

---
+++ TwoWheelTrader/Data/GlobalConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehEvalu8.Data
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool SQL, bool CSV)
+        {
+            if (SQL)
+            {
+                // TODO - CREATE SQL CONNECTION
+            }
+
+            if (CSV)
+            {
+                // TODO - CREATE CSV CONNECTION
+            }
+        }
+    }
+}
---
+++ TwoWheelTrader/Data/SqlConnector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwoWheelTrader.Models.Interfaces;
+
+namespace VehEvalu8.Data
+{
+    public class SqlConnector : IDataConnection
+    {
+        public IMotorcycle CreateMotorcycle(IMotorcycle motorcycle)
+        {
+            
+        }
+    }
+}
