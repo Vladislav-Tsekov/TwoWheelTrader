5fb88698c21b95b46ab0887e68cb6452521d0644
Vladislav <tsekov.vladislav@gmail.com>
2023-10-20 22:36:44 +0300

    Updated "MarketPrice" for Enduro/GASGAS.

--- TwoWheelTrader/Models/Enduro/GASGAS.cs
+++ TwoWheelTrader/Models/Enduro/GASGAS.cs
@@ -19,40 +19,12 @@ namespace TwoWheelTrader.Models.Enduro
 
             switch (Year)
             {
-                case 2007:
-                    MarketPrice = 5800; break;
-                case 2008:
-                    MarketPrice = 6200; break;
-                case 2009:
-                    MarketPrice = 6500; break;
-                case 2010:
-                    MarketPrice = 6800; break;
-                case 2011:
-                    MarketPrice = 7200; break;
-                case 2012:
-                    MarketPrice = 7500; break;
-                case 2013:
-                    MarketPrice = 8000; break;
-                case 2014:
-                    MarketPrice = 8400; break;
-                case 2015:
-                    MarketPrice = 9000; break;
-                case 2016:
-                    MarketPrice = 9800; break;
-                case 2017:
-                    MarketPrice = 10500; break;
-                case 2018:
-                    MarketPrice = 11000; break;
-                case 2019:
-                    MarketPrice = 12000; break;
-                case 2020:
-                    MarketPrice = 13000; break;
                 case 2021:
-                    MarketPrice = 14500; break;
+                    MarketPrice = 12000; break;
                 case 2022:
-                    MarketPrice = 15500; break;
+                    MarketPrice = 13000; break;
                 case 2023:
-                    MarketPrice = 17000; break;
+                    MarketPrice = 15000; break;
             }
 
             Profit = MarketPrice - TotalCost;
