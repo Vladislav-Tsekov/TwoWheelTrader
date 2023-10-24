690ea2ffac0b76c5b9154e426ad9c7f1e785f971
Vladislav <tsekov.vladislav@gmail.com>
2023-10-24 23:26:23 +0300

    Created few simple tests.

--- TwoWheelTrader/Core/Engine.cs
+++ TwoWheelTrader/Core/Engine.cs
@@ -32,11 +32,6 @@ namespace TwoWheelTrader.Core
                     }
                     else if (command == "add")
                     {
-                        // EXAMPLE INPUT: [1] = YAMAHA / [2] = YZF / [3] = 250 / [4] = 2019 / [5] = 33000 (FOREIGN PRICE - PRE-CONVERT) / [6] = www.blocket.com/...
-
-                        // IMPLEMENTED MOTOCROSS MODELS
-                        // IMPLEMENTED ENDURO MODELS
-
                         string make = input[1];
                         string model = input[2];
                         int cc = int.Parse(input[3]);
--- UnitTests/CoreTests/EngineTests.cs
+++ UnitTests/CoreTests/EngineTests.cs
@@ -6,5 +6,38 @@ using TwoWheelTrader.Models.Interfaces;
 
 public class EngineTests
 {
+    [SetUp]
+    public void Setup()
+    {
+        Controller controller = new();
+        Engine engine = new(controller);
+    }
 
+    [Test]
+    public void ControllerInitializesCorrectly()
+    {
+        Controller controller = new();
+        Assert.IsNull(controller);
+    }
+
+    [Test]
+    public void CreateMotorcycle_ValidInput_CreatesMotorcycle()
+    {
+        // Arrange
+        string make = "yam";
+        string model = "yzf";
+        int cc = 250;
+        int year = 2019;
+        int foreignPrice = 33000;
+        string link = "www.blocket.com";
+        int distance = 10;
+
+        Controller controller = new();
+        Engine engine = new(controller);
+
+        // Act
+        IMotorcycle motorcycle = controller.CreateMotorcycle(make, model, cc, year, foreignPrice, link, distance);
+
+        Assert.IsNotNull(motorcycle);
+    }
 }
