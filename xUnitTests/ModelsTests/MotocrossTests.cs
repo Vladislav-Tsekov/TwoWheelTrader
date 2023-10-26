2d27c1d0d8d365ff414df01dcfbfcab8887524d0
Vladislav <tsekov.vladislav@gmail.com>
2023-10-26 23:54:44 +0300

    Implemented Motocross models properties tests.

--- xUnitTests/ModelsTests/MotocrossTests.cs
+++ xUnitTests/ModelsTests/MotocrossTests.cs
@@ -43,6 +43,60 @@ namespace xUnitTests.ModelsTests
             Assert.Equal(7300, testYamaha1.MarketPrice);
             Assert.Equal(5600, testYamaha2.MarketPrice);
         }
+
+        [Fact]
+        public void InternalMotocrossModelsPropertiesMustHaveCorrectValues()
+        {
+            // Arrange
+            string model = "YZF";
+            int cc = 250;
+            int year = 2023;
+            double priceForeign = 45000;
+            string link = "www.yamaha.com";
+            int distance = 25;
+
+            // Act
+            var testYamaha = new Yamaha(model, cc, year, priceForeign, link, distance);
+
+            // Assert
+            double expectedMarketPrice = GetExpectedMarketPrice(year);
+            Assert.Equal(expectedMarketPrice, testYamaha.MarketPrice);
+            double expectedPriceBGN = priceForeign * IMotorcycle.exchangeRateSEK;
+            double expectedFuelCost = (double)(distance * 2 / 100.0) * (IMotorcycle.dieselPriceBGN * 11);
+            double expectedTotalCost = expectedPriceBGN + expectedFuelCost + IMotorcycle.commission;
+            Assert.Equal(expectedPriceBGN, testYamaha.PriceBGN);
+            Assert.Equal(expectedFuelCost, testYamaha.FuelCost);
+            Assert.Equal(expectedTotalCost, testYamaha.TotalCost);
+            double expectedProfit = expectedMarketPrice - expectedTotalCost;
+            Assert.Equal(expectedProfit, testYamaha.Profit);
+            double expectedROI = (expectedProfit / expectedTotalCost) * 100.0;
+            Assert.Equal(expectedROI, testYamaha.ROI);
+        }
+
+        private static double GetExpectedMarketPrice(int year)
+        {
+            switch (year)
+            {
+                case 2007: return 5000;
+                case 2008: return 5100;
+                case 2009: return 5200;
+                case 2010: return 5300;
+                case 2011: return 5600;
+                case 2012: return 5900;
+                case 2013: return 6200;
+                case 2014: return 6600;
+                case 2015: return 6800;
+                case 2016: return 7300;
+                case 2017: return 7800;
+                case 2018: return 8400;
+                case 2019: return 9100;
+                case 2020: return 9800;
+                case 2021: return 10500;
+                case 2022: return 11000;
+                case 2023: return 12000;
+                default: return 0;
+            }
+        }
     }
 }
 
