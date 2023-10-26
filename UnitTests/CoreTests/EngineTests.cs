0513d3495479dd590337d33680628fe15d2f1c26
Vladislav <tsekov.vladislav@gmail.com>
2023-10-26 22:19:41 +0300

    Converted from NUnit to xUnit tests.

--- UnitTests/CoreTests/EngineTests.cs
+++ UnitTests/CoreTests/EngineTests.cs
@@ -1,10 +1,10 @@
-﻿namespace UnitTests.CoreTests;
-
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using TwoWheelTrader.Core;
 using TwoWheelTrader.Core.Interfaces;
 using TwoWheelTrader.Models.Interfaces;
 
+namespace UnitTests.CoreTests
+{
 public class EngineTests
 {
     [SetUp]
@@ -12,13 +12,43 @@ public class EngineTests
     {
         Controller controller = new();
         Engine engine = new(controller);
+        engine.RunProgram();
     }
 
     [Test]
     public void ControllerInitializesCorrectly()
     {
         Controller controller = new();
-        Assert.IsNull(controller);
+        Engine engine = new(controller);
+
+        string make = "yamaha";
+        string model = "yzf";
+        int cc = 250;
+        int year = 2023;
+        int foreignPrice = 8000;
+        string link = "https://example.com";
+        int distance = 100;
+
+        // Act
+        var motorcycle = controller.CreateMotorcycle(make, model, cc, year, foreignPrice, link, distance);
+
+        // Assert
+        Assert.IsNotNull(motorcycle);
+        Assert.AreEqual(make, motorcycle.Make);
+        Assert.AreEqual(model, motorcycle.Model);
+    }
+
+    [Test]
+    public void DestinationExists_ExistingDestination_ReturnsDistance()
+    {
+        // Arrange
+        var controller = new Controller();
+
+        // Act
+        int distance = controller.DestinationExists("Linkoping");
+
+        // Assert
+        Assert.AreEqual(100, distance);
     }
 
     [Test]
@@ -41,4 +71,5 @@ public class EngineTests
 
         Assert.IsNotNull(motorcycle);
     }
+}
 }
---
+++ xUnitTests/UnitTest1.cs
@@ -0,0 +1,59 @@
+using TwoWheelTrader.Core;
+using Xunit;
+
+namespace UnitTests.CoreTests
+{
+    public class EngineTests
+    {
+        [Fact]
+        public void ControllerInitializesCorrectly()
+        {
+            // Arrange
+            Controller controller = new Controller();
+            string make = "yamaha";
+            string model = "yzf";
+
+            // Act
+            var motorcycle = controller.CreateMotorcycle(make, model, 250, 2023, 8000, "https://example.com", 100);
+
+            // Assert
+            Assert.NotNull(motorcycle);
+            Assert.Equal(make, motorcycle.Make);
+            Assert.Equal(model, motorcycle.Model);
+        }
+
+        [Fact]
+        public void DestinationExists_ExistingDestination_ReturnsDistance()
+        {
+            // Arrange
+            var controller = new Controller();
+
+            // Act
+            int distance = controller.DestinationExists("Linkoping");
+
+            // Assert
+            Assert.Equal(100, distance);
+        }
+
+        [Fact]
+        public void CreateMotorcycle_ValidInput_CreatesMotorcycle()
+        {
+            // Arrange
+            string make = "yam";
+            string model = "yzf";
+            int cc = 250;
+            int year = 2019;
+            int foreignPrice = 33000;
+            string link = "www.blocket.com";
+            int distance = 10;
+
+            Controller controller = new Controller();
+
+            // Act
+            var motorcycle = controller.CreateMotorcycle(make, model, cc, year, foreignPrice, link, distance);
+
+            // Assert
+            Assert.NotNull(motorcycle);
+        }
+    }
+}
