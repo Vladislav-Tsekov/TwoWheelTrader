8feb503dd1862c1c1e8f583adf1212ec7550f2cf
Vladislav <tsekov.vladislav@gmail.com>
2023-10-30 18:48:46 +0200

    Implemented "DBContext" for testing purposes.

---
+++ TwoWheelTrader/Data/MotoDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehEvalu8.Data
+{
+    public class MotoDbContext : DbContext
+    {
+        public DbSet<Motorcycle> Motorcycles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=YourDatabaseName;Trusted_Connection=True;"
+);
+        }
+
+        public MotoDbContext(DbContextOptions<MotoDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
