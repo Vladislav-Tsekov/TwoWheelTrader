d5ed2433e133b92637b98c14f58405309275e38e
Vladislav <tsekov.vladislav@gmail.com>
2023-10-30 18:39:51 +0200

    Implemented "Motorcycle" class for DB testing purposes.

---
+++ TwoWheelTrader/Data/Motorcycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehEvalu8.Data
+{
+    public class Motorcycle
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int CC { get; set; }
+        public int Year { get; set; }
+        public double PriceForeign { get; set; }
+        public double PriceBGN { get; set; }
+        public string Link { get; set; }
+        public int DistanceToPickUp { get; set; }
+    }
+}
