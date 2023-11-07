8553a2bfeb258d5a6e821daaa907b2f1a01237f9
Vladislav <tsekov.vladislav@gmail.com>
2023-11-07 22:28:50 +0200

    Implemented a new DBModel - Enduro.cs.

---
+++ TwoWheelTrader/Data/DBModels/Enduro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VehEvalu8.Data.DBModels;
+
+public partial class Enduro
+{
+    public int Id { get; set; }
+
+    public int? MakeId { get; set; }
+
+    public int? ModelId { get; set; }
+
+    public int? Ccid { get; set; }
+
+    public int? YearId { get; set; }
+
+    public decimal? PriceForeign { get; set; }
+
+    public decimal? PriceBgn { get; set; }
+
+    public int? Distance { get; set; }
+
+    public decimal? FuelCost { get; set; }
+
+    public decimal? TotalCost { get; set; }
+
+    public decimal? Profit { get; set; }
+
+    public decimal? Roi { get; set; }
+
+    public string? Link { get; set; }
+
+    public virtual Cc? Cc { get; set; }
+
+    public virtual Make? Make { get; set; }
+
+    public virtual Model? Model { get; set; }
+
+    public virtual Year? Year { get; set; }
+}
