6f284b55aa590e62869efebac33d13491e9ce22c
Vladislav <tsekov.vladislav@gmail.com>
2023-11-23 20:35:35 +0200

    Removed unnecessary usings for MotocrossTests.

--- VehEvalu8/Repositories/Interfaces/IRepository.cs
+++ VehEvalu8/Repositories/Interfaces/IRepository.cs
@@ -14,5 +14,13 @@ namespace VehEvalu8.Repositories.Interfaces
         //void RemoveMotorcycle(string motorcycle); -- Not yet implemented, signature unknown, probably an ID
 
         //void MotorcycleExists(T motorcycle); -- Not sure if this will be needed for the scope of the program, will just add it for now
+
+        //TODO - METHODS TO IMPLEMENT ON A LATER STAGE - ASYNC
+
+        //Task<T> GetByIdAsync<T>(int id) where T : class;
+        //Task AddAsync<T>(T entity) where T : class; 
+        //Task AddRangeAsync<T>(IEnumerable<T> entitis) where T : class; 
+        //Task DeleteAsync<T>(int id) where T : class;
+        //Task SaveChangesAsync();
     }
 }
