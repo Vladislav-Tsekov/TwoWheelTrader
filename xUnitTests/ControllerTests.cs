4137aa8f2532d71c273a0c516fa4eb2c27814200
Vladislav <tsekov.vladislav@gmail.com>
2023-12-08 23:14:49 +0200

    Started implementing Controller xUnit tests.

--- xUnitTests/ControllerTests.cs
+++ xUnitTests/ControllerTests.cs
@@ -1,14 +1,31 @@
-﻿using VehEvalu8.Core;
-using VehEvalu8.Data.DBModels;
+﻿using Moq;
+using VehEvalu8.Core;
 using VehEvalu8.Data;
+using VehEvalu8.Data.DBModels;
+using VehEvalu8.Models.Interfaces;
 using VehEvalu8.Repositories.Interfaces;
-using Moq;
 using Xunit;
 
 namespace xUnitTests
 {
     public class ControllerTests
     {
+        [Fact]
+        public void CreateMotorcycleMustCallRepository()
+        {
+            // Arrange
+            var mockEnduroRepository = new Mock<IRepository<Enduro>>();
+            var mockMotocrossRepository = new Mock<IRepository<Motocross>>();
+            var mockContext = new Mock<MotoContext>();
+        }
 
+        [Fact]
+        public void GetMotorcycleInfoAsyncMustReturnsData()
+        {
+            // Arrange
+            var mockEnduroRepository = new Mock<IRepository<Enduro>>();
+            var mockMotocrossRepository = new Mock<IRepository<Motocross>>();
+            var mockContext = new Mock<MotoContext>();
+        }
     }
 }
